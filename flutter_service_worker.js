7b63024f6331d365aa7808bff57f0b93adc78103
Sanjay <164481795+sanjaytamilan0@users.noreply.github.com>
2025-09-22 11:21:26 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "158195e8933bcd64e0e205eab8ccbb4f
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "0781679c15c40dbeaca3a816148a509b",
+"flutter_bootstrap.js": "123106dbc8c03b4a91e8960ba03a19ae",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
